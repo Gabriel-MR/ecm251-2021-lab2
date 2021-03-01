db01d39f83dac2177bbb63906295227ff155a952
Gabriel-MR <gabss.moreira@icloud.com>
2021-03-01 09:21:22 -0300

    Códigos básicos

---
+++ Aula 02/src/silva/moreira/gabriel/Basico01.java
@@ -0,0 +1,24 @@
+package silva.moreira.gabriel;
+
+public class Basico01 {
+    public static void main(String[] args) {
+        int ano_nascimento;
+        float salario;
+        double cosmo;
+        boolean despertouSetimoSentido;
+        String nome;
+
+        ano_nascimento = 1986;
+        salario = 1999.99f;
+        cosmo = 7000;
+        despertouSetimoSentido = false;
+        nome = "Ask ketchun";
+
+        System.out.println("Nome: " +nome);
+        System.out.println("Salario: " +salario + "Zeni");
+
+
+
+
+;    }
+}
---
+++ Aula 02/src/silva/moreira/gabriel/Main.java
@@ -0,0 +1,9 @@
+package silva.moreira.gabriel;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Ola Mundo do Intellij");
+    }
+}
