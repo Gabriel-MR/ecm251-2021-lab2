725799407a45202d0e72a5fe1f36cc7622f8566b
Gabriel-MR <gabss.moreira@icloud.com>
2021-03-18 09:16:22 -0300

    Validador CPF

---
+++ ValidadorCPF/src/silva/moreira/gabriel/Main.java
@@ -0,0 +1,9 @@
+package silva.moreira.gabriel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String cpf = "487.045.698-22";
+        System.out.println("Cpf valido:" + ValidadorCPF.validarCpf(cpf));
+    }
+}
