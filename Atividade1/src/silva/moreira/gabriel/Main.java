ccdfd253a3b69626706a7631769eebd35803cef7
Gabriel-MR <gabss.moreira@icloud.com>
2021-03-31 17:20:30 -0300

    Classe Usuários

---
+++ Atividade1/src/silva/moreira/gabriel/Main.java
@@ -0,0 +1,11 @@
+//Gabriel Moreira Silva - 19.00397-8
+//Felipe Oliveira Boacnin - 19.00616-0
+
+package silva.moreira.gabriel;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ Atividade1/src/silva/moreira/gabriel/Usuarios.java
@@ -0,0 +1,25 @@
+//Gabriel Moreira Silva - 19.00397-8
+//Felipe Oliveira Boacnin - 19.00616-0
+
+package silva.moreira.gabriel;
+
+public class Usuarios {
+    private String nome;
+    private String senha;
+    private String email;
+
+    public Usuarios(String nome){
+        this.nome = nome;
+        this. senha = senha;
+        this.email = email;
+    }
+
+    @Override
+    public String toString(){
+        return "Usuario{" +
+                "nome='" + nome + '\'' +
+                ", senha='" + senha + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
