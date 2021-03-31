16330b861c28ea01df61c54670bceb4cb28a13c8
Gabriel-MR <gabss.moreira@icloud.com>
2021-03-31 17:28:12 -0300

    Classe Contas

---
+++ Atividade1/src/silva/moreira/gabriel/Contas.java
@@ -0,0 +1,13 @@
+package silva.moreira.gabriel;
+
+public class Contas {
+    private Usuarios usuario;
+    private int idConta;
+    private double saldo;
+
+    public Contas{
+        this.usuario = usuario;
+        this.idConta = idConta;
+        this.saldo = saldo;
+    }
+}
