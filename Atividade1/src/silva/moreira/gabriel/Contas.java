0856596a585dd78ad49188148b9e681c8ec5678f
Gabriel-MR <gabss.moreira@icloud.com>
2021-03-31 18:01:53 -0300

    Update Contas

--- Atividade1/src/silva/moreira/gabriel/Contas.java
+++ Atividade1/src/silva/moreira/gabriel/Contas.java
@@ -1,3 +1,6 @@
+//Gabriel Moreira Silva - 19.00397-8
+//Felipe Oliveira Boacnin - 19.00616-0
+
 package silva.moreira.gabriel;
 
 public class Contas {
@@ -5,9 +8,29 @@ public class Contas {
     private int idConta;
     private double saldo;
 
-    public Contas{
-        this.usuario = usuario;
+    public Contas(String nome, int idConta, double saldo) {
+        this.usuario = new Usuarios(nome);
         this.idConta = idConta;
         this.saldo = saldo;
     }
+
+    public void depositar(double valor){
+        this.saldo += valor;
+    }
+
+    public boolean sacar(double valor) {
+        if (valor <= this.saldo) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+        public boolean transferir(Contas destino, double valor){
+            if(sacar(valor)){
+                destino.depositar(valor);
+                return true;
+            }
+            return false;
+        }
 }
