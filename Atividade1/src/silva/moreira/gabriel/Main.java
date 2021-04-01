f43bdf8b89e58140d79666caaff2829f36ffc4aa
Gabriel-MR <gabss.moreira@icloud.com>
2021-04-01 17:36:41 -0300

    Arrumando as transações

--- Atividade1/src/silva/moreira/gabriel/Main.java
+++ Atividade1/src/silva/moreira/gabriel/Main.java
@@ -7,25 +7,28 @@ package silva.moreira.gabriel;
 public class Main {
 
     public static void main(String[] args) {
+
 	    Usuarios usuario1 = new Usuarios("All Might");
         Usuarios usuario2 = new Usuarios("One For All");
         Usuarios usuario3 = new Usuarios("Bakugo");
 
-        Contas conta1 = new Contas(usuario1,Transacoes.qrCode(),1000);
-        Contas conta2 = new Contas(usuario2, Transacoes.qrCode(),250);
-        Contas conta3 = new Contas(usuario3, Transacoes.qrCode(),3000);
+        Contas conta1 = new Contas(usuario1, 1,1000.00);
+        Contas conta2 = new Contas(usuario2, 2,250.00);
+        Contas conta3 = new Contas(usuario3,3,3000.00);
 
         System.out.println("Estado inicial:");
         System.out.println("Conta 1: " + conta1.toString());
         System.out.println("Conta 2: " + conta2.toString());
         System.out.println("Conta 3: " + conta3.toString());
 
-        String operacao1 = Transacoes.qrCode(conta1, 250);
+        Transacoes transacoes = new Transacoes();
+
+        String operacao1 = transacoes.qrCode(conta1, 250.00);
         // Usuário 2 paga a requisição do usuário 1
         // Usuário 3 paga a requisição do usuário 1
         // Usuário 2 paga a requisição do usuário 1
 
-        String operacao2 = Transacoes.qrCode(conta2, 1000);
+        String operacao2 = transacoes.qrCode(conta2, 1000.00);
         // Usuário 3 paga a requisição do usuário 2
 
         System.out.println("Estado Final:");
