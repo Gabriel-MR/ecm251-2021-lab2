aa8cf9cb1721c8903aaa9e4f50b7d9893f79206d
Gabriel-MR <gabss.moreira@icloud.com>
2021-04-01 13:01:01 -0300

    Update Transacoes e random

--- Atividade1/src/silva/moreira/gabriel/Transacoes.java
+++ Atividade1/src/silva/moreira/gabriel/Transacoes.java
@@ -1,8 +1,15 @@
-
 //Gabriel Moreira Silva - 19.00397-8
 //Felipe Oliveira Boacnin - 19.00616-0
 
 package silva.moreira.gabriel;
 
+import java.util.Random;
+
 public class Transacoes {
+
+    private static int getRandomNumberInRange(int min, int max) {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+
 }
