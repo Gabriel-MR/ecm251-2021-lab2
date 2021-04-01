fc546cff09b4ab629868a99266dd68e74e626337
Gabriel-MR <gabss.moreira@icloud.com>
2021-04-01 12:58:34 -0300

    Update Contas e getters

--- Atividade1/src/silva/moreira/gabriel/Contas.java
+++ Atividade1/src/silva/moreira/gabriel/Contas.java
@@ -4,22 +4,34 @@
 
 package silva.moreira.gabriel;
 
-public class Contas {
+public class Contas{
     private Usuarios usuario;
     private int idConta;
     private double saldo;
 
-    public Contas(String nome, int idConta, double saldo) {
-        this.usuario = new Usuarios(nome);
+    public Contas(Usuarios usuario, int idConta, double saldo){
+        this.usuario = usuario;
         this.idConta = idConta;
         this.saldo = saldo;
+    }
+
+    public Usuarios getUsuario(){
+        return usuario;
+    }
+
+    public int getIdConta(){
+        return idConta;
+    }
+
+    public double getSaldo(){
+        return saldo;
     }
 
     public void depositar(double valor){
         this.saldo += valor;
     }
 
-    public boolean sacar(double valor) {
+    public boolean sacar(double valor){
         if (valor <= this.saldo) {
             this.saldo -= valor;
             return true;
@@ -27,11 +39,20 @@ public class Contas {
         return false;
     }
 
-        public boolean transferir(Contas destino, double valor){
-            if(sacar(valor)){
-                destino.depositar(valor);
-                return true;
-            }
-            return false;
+    public boolean transferir(Contas destino, double valor){
+        if(sacar(valor)){
+            destino.depositar(valor);
+            return true;
         }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Contas{" +
+                "usuario=" + usuario +
+                ", idConta=" + idConta +
+                ", saldo=" + saldo +
+                '}';
+    }
 }
