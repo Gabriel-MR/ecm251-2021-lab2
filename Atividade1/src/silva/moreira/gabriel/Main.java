c2587d9e19ebf37f7e80f7c307d9cfcdc34e3027
Gabriel-MR <gabss.moreira@icloud.com>
2021-04-01 12:47:11 -0300

    Update Conta

--- Atividade1/src/silva/moreira/gabriel/Main.java
+++ Atividade1/src/silva/moreira/gabriel/Main.java
@@ -1,3 +1,4 @@
+
 //Gabriel Moreira Silva - 19.00397-8
 //Felipe Oliveira Boacnin - 19.00616-0
 
@@ -6,6 +7,17 @@ package silva.moreira.gabriel;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Usuarios Gohan = new Usuarios("Gohan");
+        Usuarios Goten = new Usuarios("Goten");
+        Usuarios Trunks = new Usuarios("Trunks");
+
+        Contas conta1 = new Contas(Gohan, 1, 1000);
+        Contas conta2 = new Contas(Goten, 2, 250);
+        Contas conta3 = new Contas(Trunks, 3, 3000);
+
+
+
+
     }
 }
--- Atividade1/src/silva/moreira/gabriel/Usuarios.java
+++ Atividade1/src/silva/moreira/gabriel/Usuarios.java
@@ -1,22 +1,27 @@
+
 //Gabriel Moreira Silva - 19.00397-8
 //Felipe Oliveira Boacnin - 19.00616-0
 
 package silva.moreira.gabriel;
 
 public class Usuarios {
-    private String nome;
+    private Usuarios nome;
     private String senha;
     private String email;
 
-    public Usuarios(String nome){
+    public Usuarios(Usuarios nome){
         this.nome = nome;
         this. senha = senha;
         this.email = email;
     }
 
+    public Usuarios getNome(){
+        return nome;
+    }
+
     @Override
-    public String toString(){
-        return "Usuario{" +
+    public String toString() {
+        return "Usuarios{" +
                 "nome='" + nome + '\'' +
                 ", senha='" + senha + '\'' +
                 ", email='" + email + '\'' +
