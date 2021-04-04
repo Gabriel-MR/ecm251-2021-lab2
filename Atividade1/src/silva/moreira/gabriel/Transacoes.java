ec84b67985ad4e83ec890666ab3c0e7b99f5070d
Gabriel-MR <gabss.moreira@icloud.com>
2021-04-04 13:23:26 -0300

    Comentários Transações

--- Atividade1/src/silva/moreira/gabriel/Transacoes.java
+++ Atividade1/src/silva/moreira/gabriel/Transacoes.java
@@ -7,22 +7,28 @@ import java.util.Random;
 
 public class Transacoes {
 
+    // Gerar um número aleatório
     private static int getRandomNumberInRange(int min, int max) {
         Random r = new Random();
         return r.nextInt((max - min) + 1) + min;
     }
-    
-    public String qrCode(Contas conta, double valor){
-        return conta.getIdConta() + ";" + conta.getUsuario().getNome() + ";" + valor + ";" +  getRandomNumberInRange(1000,9999);
 
+    // Gerar o qrCode em string
+    public String qrCode(Contas conta, double valor) {
+        return conta.getIdConta() +
+                ";" + conta.getUsuario().getNome() +
+                ";" + valor + ";" +
+                getRandomNumberInRange(1000, 9999);
     }
+
+    // Gerar um pagamento/transferência
     public boolean pagamento(Contas pagador, Contas recebedor, String s) {
         String[] dados = s.split(";");
-        //Verificar se é possível fazer a transferência
-        //if( ){
+        // Código debatido com Gabriel Domingues Garofalo – 19.01229-2
+        if ((recebedor.getUsuario().getNome().equals(dados[1])) && (Integer.toString(recebedor.getIdConta()).equals(dados[0])) && (pagador.getSaldo() >= Double.parseDouble(dados[2]))) {
             pagador.transferir(recebedor, Double.parseDouble(dados[2]));
             return true;
         }
-        //return false;
-
+        return false;
+    }
 }
