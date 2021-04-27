44dc76c57fe22b4c36887848cf7dd5e5728dce9d
Gabriel-MR <gabss.moreira@icloud.com>
2021-04-26 21:18:56 -0300

    Refatoração do projeto

---
+++ Aula08-Lab/pedra-papel-tesoura-2/src/silva/moreira/gabriel/pedra_papel_tesoura/Main.java
@@ -0,0 +1,11 @@
+package silva.moreira.gabriel.pedra_papel_tesoura;
+
+import silva.moreira.gabriel.pedra_papel_tesoura.controlador.Sistema;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Sistema sistema = new Sistema();
+        sistema.run();
+    }
+}
---
+++ Aula08-Lab/pedra-papel-tesoura-2/src/silva/moreira/gabriel/pedra_papel_tesoura/enumeracao/Resultado.java
@@ -0,0 +1,5 @@
+package silva.moreira.gabriel.pedra_papel_tesoura.enumeracao;
+
+public enum Resultado {
+    Perdeu, Empatou, Ganhou;
+}
