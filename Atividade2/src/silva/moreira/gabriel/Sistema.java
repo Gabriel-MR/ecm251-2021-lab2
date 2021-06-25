5372652a09397edd2d59f265469a3384cc4c4b33
Gabriel-MR <gabss.moreira@icloud.com>
2021-06-25 20:18:35 -0300

    indentação do sistema

--- Atividade2/src/silva/moreira/gabriel/Sistema.java
+++ Atividade2/src/silva/moreira/gabriel/Sistema.java
@@ -30,55 +30,55 @@ public class Sistema {
 
         do {
             switch (op){
+                case 0:
+                    System.exit();
                 case 1:
-                System.out.println("Cargo do membro: \n");
-                System.out.println("1 - Mobile Member");
-                System.out.println("2 - Heavy Lifter");
-                System.out.println("3 - Script Guy");
-                System.out.println("4 - Big Brother");
-                int cargo = scanner.nextInt();
-                switch (cargo){
-                    case 0:
-                        System.exit();
-                    case 1:
-                        System.out.println("Nome: ");
-                        String nome1 = scanner.next();
-                        System.out.println("Id: ");
-                        String email1 = scanner.next();
-                        mobileMembers = new MobileMembers(nome1, email1);
+                    System.out.println("Cargo do membro: \n");
+                    System.out.println("1 - Mobile Member");
+                    System.out.println("2 - Heavy Lifter");
+                    System.out.println("3 - Script Guy");
+                    System.out.println("4 - Big Brother");
+                    int cargo = scanner.nextInt();
+                    switch (cargo){
+                        case 1:
+                            System.out.println("Nome: ");
+                            String nome1 = scanner.next();
+                            System.out.println("Id: ");
+                            String email1 = scanner.next();
+                            mobileMembers = new MobileMembers(nome1, email1);
 
-                        break;
-                    case 2:
-                        System.out.println("Nome: ");
-                        String nome2 = scanner.next();
-                        System.out.println("Id: ");
-                        String email2 = scanner.next();
-                        heavyLifters = new HeavyLifters(nome2, email2);
+                            break;
+                        case 2:
+                            System.out.println("Nome: ");
+                            String nome2 = scanner.next();
+                            System.out.println("Id: ");
+                            String email2 = scanner.next();
+                            heavyLifters = new HeavyLifters(nome2, email2);
 
-                        break;
+                            break;
 
-                    case 3:
-                        System.out.println("Nome: ");
-                        String nome3 = scanner.next();
-                        System.out.println("Id: ");
-                        String email3 = scanner.next();
-                        scriptGuys = new ScriptGuys(nome3, email3);
+                        case 3:
+                            System.out.println("Nome: ");
+                            String nome3 = scanner.next();
+                            System.out.println("Id: ");
+                            String email3 = scanner.next();
+                            scriptGuys = new ScriptGuys(nome3, email3);
 
-                        break;
+                            break;
 
-                    case 4:
-                        System.out.println("Nome: ");
-                        String nome4 = scanner.next();
-                        System.out.println("Id: ");
-                        String email4 = scanner.next();
-                        bigBrothers = new BigBrothers(nome4, email4);
+                        case 4:
+                            System.out.println("Nome: ");
+                            String nome4 = scanner.next();
+                            System.out.println("Id: ");
+                            String email4 = scanner.next();
+                            bigBrothers = new BigBrothers(nome4, email4);
 
-                        break;
+                            break;
 
-                    default:
-                        System.out.println("Cargo invalido");
-                }
-                menu();
+                        default:
+                            System.out.println("Cargo invalido");
+                    }
+                    menu();
 
                 case 2:
                     System.out.println("nome: ");
