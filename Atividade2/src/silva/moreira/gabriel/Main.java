0ce2bcd2187d2b68ec1dc84698b25333a50b744f
Gabriel-MR <gabss.moreira@icloud.com>
2021-06-25 20:33:35 -0300

    Atualização main

--- Atividade2/src/silva/moreira/gabriel/Main.java
+++ Atividade2/src/silva/moreira/gabriel/Main.java
@@ -1,8 +1,11 @@
 package silva.moreira.gabriel;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Sistema sistema = new Sistema();
+        sistema.menu();
     }
 }
