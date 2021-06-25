c1114955d6c8b2c83c10cd9a720b3396e9b0ade6
Gabriel-MR <gabss.moreira@icloud.com>
2021-06-25 16:21:12 -0300

    Opção de registrar membros

--- Atividade2/src/silva/moreira/gabriel/Sistema.java
+++ Atividade2/src/silva/moreira/gabriel/Sistema.java
@@ -15,20 +15,69 @@ public class Sistema {
 
     public void menu(){
         System.out.println("Bem-vindo!");
-        System.out.println("Opções: ");
         System.out.println("1 - Registrar membros");
         System.out.println("2 - Horário de trabalho");
         System.out.println("3 - Postar mensagem");
         System.out.println("4 - Mudar horário de trabalho");
         System.out.println("0 - Sair");
+        int op = scanner.nextInt();
 
-    int op = scanner.nextInt();
-    do {
-        switch (op){
-            case 1:
+        do {
+            switch (op){
+                case 1:
+                System.out.println("Cargo do membro: \n");
+                System.out.println("1 - Mobile Member");
+                System.out.println("2 - Heavy Lifter");
+                System.out.println("3 - Script Guy");
+                System.out.println("4 - Big Brother");
+                int cargo = scanner.nextInt();
+                switch (cargo){
+                    case 1:
+                        System.out.println("Nome: ");
+                        String nome = scanner.next();
+                        System.out.println("Id: ");
+                        int id = scanner.nextInt();
+                        mobileMembers = new MobileMembers(nome, id);
 
-        }
-    }while (op != 0);
+                        break;
+                    case 2:
+                        System.out.println("Nome: ");
+                        String nome = scanner.next();
+                        System.out.println("Id: ");
+                        int id = scanner.nextInt();
+                        heavyLifters = new HeavyLifters(nome, id);
+
+                        break;
+
+                    case 3:
+                        System.out.println("Nome: ");
+                        String nome = scanner.next();
+                        System.out.println("Id: ");
+                        int id = scanner.nextInt();
+                        scriptGuys = new ScriptGuys(nome, id);
+
+                        break;
+
+                    case 4:
+                        System.out.println("Nome: ");
+                        String nome = scanner.next();
+                        System.out.println("Id: ");
+                        int id = scanner.nextInt();
+                        bigBrothers = new BigBrothers(nome, id);
+
+                        break;
+
+                    default:
+                        System.out.println("Cargo invalido");
+                }
+                menu();
+
+                case 2:
+                    System.out.println("");
+
+
+            }
+        }while (op != 0);
     }
 }
 
